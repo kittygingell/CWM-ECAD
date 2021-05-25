367f331ae7adbea786786957d83311949faba706
Cloud User <centos@ip-172-31-40-84.eu-west-2.compute.internal>
2021-05-25 15:41:34 +0000

    A complete solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Katrina Gingell
+// Date: 25.05.21
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -15,4 +15,29 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module lighting(
+	input rst,
+	input clk,
+	input button,
+	output reg [2:0]colour);
+
+initial begin
+	colour=1;
+end
+
+always @(posedge clk)
+	begin
+	if (rst)
+		colour=0;
+	else if ((button==1) && (colour!=7) && (colour!=6)) //steady loop for 1-5
+		colour=colour+1;
+	else if (button==0 && colour!=7 && colour!=0) //constant if button is not pressed
+		colour=colour;
+	else
+		colour=1;
+	end
+
+endmodule
 
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,87 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Katrina Gingell	
+// Date: 25.05.21
 //
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module top_tb(
+);
+
+parameter CLK_PERIOD = 10;
+
+reg clk;
+reg rst;
+reg button;
+reg err;
+wire [2:0]colour;
+
+//CLOCK
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+
+initial begin
+rst=1;
+button=0;
+err=0;
+forever begin
+	//Testing reset
+	#(CLK_PERIOD) if (colour!=0)
+		begin
+		err=1;
+		end
+	
+	//Testing case of 0 or 7
+	rst=0;
+	#(CLK_PERIOD) if (colour==7 | colour==0)
+		begin 
+		err=1;
+		end
+	button=1; 
+	#(CLK_PERIOD) if (colour==7 | colour==0)
+		begin 
+		err=1;
+		end
+	end
+end
+
+
+
+//Success check
+initial begin
+#(300*CLK_PERIOD)
+	if (err==0) begin
+       		$display("PASS");
+       		$finish;
+	end
+	else
+ 		begin
+		$display("FAIL");
+		$finish;
+	end
+end
+
+
+lighting top(clk,rst,button,colour);
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
 
