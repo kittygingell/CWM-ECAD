1e4d874a8b7eb9fc48e1c73e564a61c492624cbe
Cloud User <centos@ip-172-31-40-84.eu-west-2.compute.internal>
2021-05-26 14:02:45 +0000

    A solution to exercise 5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Katrina Gingell
+// Date: 26.05.21
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -13,3 +13,43 @@
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module ac(
+	input clk, 
+	input [4:0]temperature,
+	output reg heating, 
+	output reg cooling);
+
+always @(posedge clk)
+	begin
+	//cooling
+	if (temperature>22)
+		begin
+		cooling<=1;
+		end
+	else if (temperature<=20)
+		begin
+		cooling<=0;
+		end
+	else 
+		begin
+		cooling<=cooling;
+		end
+
+	//heating
+	if (temperature<18)
+		begin
+		heating<=1;
+		end
+	else if (temperature>=20)
+		begin
+		heating<=0;
+		end
+	else 
+		begin
+		heating<=heating;
+		end
+	end
+endmodule
+	
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,80 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Katrina Gingell
+// Date: 26.05.21
 //
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module top_tb();
+
+parameter CLK_PERIOD = 10;
+
+reg clk;
+reg err;
+wire heating;
+wire cooling;
+reg [4:0]temperature;
+
+//CLOCK
+initial
+begin
+clk = 1'b0;
+	forever
+        #(CLK_PERIOD/2) clk=~clk;
+end
+
+
+initial
+begin
+//Low temperature test
+temperature = 17;
+
+forever begin
+	#(CLK_PERIOD) if ((cooling==1&&temperature<20)||(heating==1&&temperature>20))
+		begin err=1;
+		end
+		else
+		begin err=0;
+		end
+end
+
+//High temperature test
+temperature = 23;	
+forever begin
+	#(CLK_PERIOD) if ((heating==1&&temperature>20)||(cooling==1&&temperature<20)) 
+		begin err=1;
+		end
+		else
+		begin err=0;
+		end
+
+	end
+end
+
+
+//Success check
+initial begin
+#(3*CLK_PERIOD)
+	if (err==0) begin
+       		$display("PASSED");
+       		$finish;
+	end
+	else
+ 		begin
+		$display("FAILED");
+		$finish;
+	end
+end
+
+
+ac top(clk,temperature,heating,cooling);
+
+endmodule
+
+
+
 
