5ed1d003e6fc2cd733e880625cae893dfb198e47
Cloud User <centos@ip-172-31-40-84.eu-west-2.compute.internal>
2021-05-27 12:36:02 +0000

    Exercise 6

--- Ex6/top.v
+++ Ex6/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Katrina Gingell
+// Date: 27.05.21
 //
 //
 //  Description: In this exercise, you need to design a memory with 8 entries, 
@@ -14,4 +14,33 @@
 //           rgb [23:0]
 //
 //////////////////////////////////////////////////////////////////////////////////
+
+
+`timescale 1ns / 100ps
+
+module RGB(
+	input clk,
+	input [2:0]colour,
+	input enable,
+	output [23:0]rgb);
+
+
+mybram your_instance_name (
+  .clka(clka),    // input wire clka
+  .ena(ena),      // input wire ena
+  .wea(wea),      // input wire [0 : 0] wea
+  .addra(addra),  // input wire [3 : 0] addra
+  .dina(dina),    // input wire [15 : 0] dina
+  .douta(douta)  // output wire [15 : 0] douta
+);
+
+
+
+endmodule
+
+
+
+
+
+
 
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -1,9 +1,85 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Katrina Gingell
+// Date: 27.05.21
 //
 // Description: A testbench module to test Ex6 - RGB Colour Converter
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb();
+
+parameter CLK_PERIOD = 10;
+
+reg clka;
+reg [2:0]colour;
+reg err;
+reg ena;
+reg [23:0]t;
+wire [23:0]rgb;
+
+//CLOCK
+initial
+begin
+clka = 1'b0;
+	forever
+        #(CLK_PERIOD/2) clka=~clka;
+end
+
+
+//Testing
+initial
+begin
+//Specific value test
+ena=1;
+err=0;
+colour=0;
+	forever
+	begin
+	#(CLK_PERIOD) if (rgb!=0)
+		begin
+		err=1;
+		end
+	else
+		begin
+		err=0;
+		end
+	end
+
+//Enable test
+ena=0;
+colour=1;
+	forever
+	begin
+	#(CLK_PERIOD) if (rgb!=0)
+		begin
+		err=1;
+		end
+	else
+		begin
+		err=err;
+		end
+end
+end
+
+
+
+//Success check
+initial begin
+#(3*CLK_PERIOD)
+	if (err==0) begin
+       		$display("PASSED");
+       		$finish;
+	end
+	else
+ 		begin
+		$display("FAILED");
+		$finish;
+	end
+end
+
+RGB top(clka, colour, ena, rgb);
+
+endmodule
